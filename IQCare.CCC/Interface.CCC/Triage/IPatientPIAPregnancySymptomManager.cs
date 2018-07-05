6fc287d899a8eb9d71010b500d56ee59d4309458
Kenneth Omondi Ochieng <omondi.ken@outlook.com>
2018-07-05 18:04:13 +0300

    Initial work done on the Pregnancy intention assessment(PIA) form

---
+++ IQCare.CCC/Interface.CCC/Triage/IPatientPIAPregnancySymptomManager.cs
@@ -0,0 +1,15 @@
+﻿using Entities.CCC.Triage;
+using System.Collections.Generic;
+
+namespace Interface.CCC.Triage
+{
+    public interface IPatientPIAPregnancySymptomManager
+    {
+        int AddPregnancySymptom(PatientPIAPregnancySymptom a);
+        int UpdatePregnancySymptom(PatientPIAPregnancySymptom u);
+        int DeletePregnancySymptom(int id);
+        int DeletePregnancySymptoms(int patientPIAId);
+        List<PatientPIAPregnancySymptom> GetPatientPregnancySymptoms(int patientPIAId);
+        int CheckIfPatientHasPregnancySymptom(int patientId);
+    }
+}
