16668147dcf8f35950d0755c7aed4066fa957dc1
Kenneth Omondi Ochieng <omondi.ken@outlook.com>
2018-07-05 18:04:13 +0300

    Initial work done on the Pregnancy intention assessment(PIA) form

---
+++ IQCare.CCC/DataAccess.CCC/Repository/Triage/PatientPregnancyIntentionAssessmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.CCC.Context;
+using DataAccess.CCC.Interface.Triage;
+using DataAccess.Context;
+using Entities.CCC.Triage;
+
+namespace DataAccess.CCC.Repository.Triage
+{
+    public class PatientPregnancyIntentionAssessmentRepository : BaseRepository<PatientPregnancyIntentionAssessment>, IPatientPregnancyIntentionAssessmentRepository
+    {
+        private GreencardContext _context;
+
+        public PatientPregnancyIntentionAssessmentRepository() : this(new GreencardContext())
+        {
+
+        }
+
+        public PatientPregnancyIntentionAssessmentRepository(GreencardContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ IQCare.CCC/IQCare.CCC.UILogic/Triage/PatientPIAPregnancySymptomManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities.CCC.Triage;
+using Application.Presentation;
+using Interface.CCC.Triage;
+
+namespace IQCare.CCC.UILogic.Triage
+{
+    public class PatientPIAPregnancySymptomManager
+    {
+        private IPatientPIAPregnancySymptomManager _PatientPIAPregnancySymptom = (IPatientPIAPregnancySymptomManager)ObjectFactory.CreateInstance("BusinessProcess.CCC.Triage.BPatientPIAPregnancySymptom, BusinessProcess.CCC");
+
+        public int AddPIAPregnancySymptom(int patientId, int patientPIAId, int pregnancySymptomId, int userId)
+        {
+            try
+            {
+                var FPLoad = new PatientPIAPregnancySymptom()
+                {
+                    PatientId = patientId,
+                    PatientPIAId = patientPIAId,
+                    PregnancySymptomId = pregnancySymptomId,
+                    CreatedBy = userId
+                };
+
+                return _PatientPIAPregnancySymptom.AddPregnancySymptom(FPLoad);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public int UpdatePIAPregnancySymptom(int PregnancySymptomId, int id)
+        {
+            try
+            {
+                var FPLoad = new PatientPIAPregnancySymptom()
+                {
+                    Id = id,
+                    PregnancySymptomId = PregnancySymptomId
+                };
+
+                return _PatientPIAPregnancySymptom.UpdatePregnancySymptom(FPLoad);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public int DeletePIAPregnancySymptom(int id)
+        {
+            try
+            {
+                return _PatientPIAPregnancySymptom.DeletePregnancySymptom(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public int DeletePregnancySymptoms(int patientPIAId)
+        {
+            try
+            {
+                return _PatientPIAPregnancySymptom.DeletePregnancySymptoms(patientPIAId);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<PatientPIAPregnancySymptom> GetPatientPIAPregnancySymptoms(int patientPIAId)
+        {
+            try
+            {
+                return _PatientPIAPregnancySymptom.GetPatientPregnancySymptoms(patientPIAId);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public int CheckIfPatientHasPIAPregnancySymptom(int patientId)
+        {
+            try
+            {
+                return _PatientPIAPregnancySymptom.CheckIfPatientHasPregnancySymptom(patientId);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
